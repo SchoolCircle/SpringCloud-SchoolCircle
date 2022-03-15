7559ff2e092da18b41865a49bda7f675a17484fe
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-03-15 20:34:39 +0800

    将Chat模块注册转移到Nacos上

--- Chat/src/main/java/com/example/ChatApplication.java
+++ Chat/src/main/java/com/example/ChatApplication.java
@@ -3,10 +3,11 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("com/example/dao")
 public class ChatApplication {
     public static void main(String[] args) {
--- Chat/src/main/java/com/example/controller/TesController.java
+++ Chat/src/main/java/com/example/controller/TesController.java
@@ -5,6 +5,7 @@ import com.example.entity.Result;
 import com.example.entity.Tes;
 import com.example.service.TesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
