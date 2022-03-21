4e27313590e8d015573ece67ef30e1fc5d11c0d9
Jiahao Fu <sunabrhh@foxmail.com>
2022-03-21 20:17:09 +0800

    对登录密码进行了加密处理

--- Register/src/main/java/com/example/service/LoginService.java
+++ Register/src/main/java/com/example/service/LoginService.java
@@ -8,6 +8,7 @@ package com.example.service;
 import com.example.entity.Result;
 import com.example.entity.User;
 import com.example.repository.LoginRepository;
+import com.example.utils.Md5;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +16,18 @@ import java.util.List;
 
 @Service
 public class LoginService {
-    @Autowired private LoginRepository loginRepository;
+    @Autowired
+    private LoginRepository loginRepository;
     public Result<List<User>> login(String email, String pass){
+        if(email.isEmpty()||pass.isEmpty())
+            return new Result<>("传入参数为空",201);
         List<User> list = loginRepository.getUserByEmail(email);
         if(list.isEmpty())
             return new Result<>("用户不存在", 201);
-        if(!pass.equals(list.get(0).getPass()))
-            return new Result<>("密码错误", 202);
+        String pas = Md5.md5(pass, email);
+        assert pas != null;
+        if(!pas.equals(list.get(0).getPass()))
+            return new Result<>("密码错误", 201);
         else return new Result<>(list);
     }
 }
---
+++ Register/src/main/java/com/example/utils/Check.java
@@ -0,0 +1,14 @@
+package com.example.utils;
+
+public class Check {
+    public static boolean checkInt(String num)
+    {
+        if(num==null) return false;
+        for(int i=0;i<num.length();i++)
+        {
+            if(num.charAt(i)<'0'||num.charAt(i)>'9')
+                return false;
+        }
+        return true;
+    }
+}
