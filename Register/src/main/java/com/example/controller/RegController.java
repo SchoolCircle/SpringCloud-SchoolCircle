2dbd932e9c7720735eb4c9a5fdb41f012f1d3f99
Jiahao Fu <sunabrhh@foxmail.com>
2022-04-11 15:05:54 +0800

    配置好网关和软路由

--- Register/src/main/java/com/example/controller/RegController.java
+++ Register/src/main/java/com/example/controller/RegController.java
@@ -4,7 +4,6 @@ import com.example.entity.Request.EmailRequest;
 import com.example.entity.Result;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
