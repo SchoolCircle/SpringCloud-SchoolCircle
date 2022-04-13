0c1c4414e905be0dda8360f48f10c956a628dc38
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-04-13 20:24:59 +0800

    登录实现

--- Register/src/main/java/com/example/controller/RegController.java
+++ Register/src/main/java/com/example/controller/RegController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
 import com.example.entity.Request.EmailRequest;
+import com.example.entity.Request.LoginRequest;
 import com.example.entity.Request.RegRequest;
 import com.example.entity.Result;
 import com.example.entity.User;
+import com.example.service.LoginService;
 import com.example.service.RegService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,20 +19,34 @@ public class RegController {
 
     @Autowired
     private RegService regService;
+
+    @Autowired
+    private LoginService loginService;
+
     @PostMapping("/sendEmail")
-    public Result<List<User>> sendEmail(@RequestBody EmailRequest emailRequest)
-    {
+    public Result<List<User>> sendEmail(@RequestBody EmailRequest emailRequest) {
         String email = emailRequest.getEmail();
         System.out.println(email);
         return regService.sendEmail(email);
     }
+
     @PostMapping("/reg")
-    public Result<List<User>> reg(@RequestBody RegRequest regRequest){
+    public Result<List<User>> reg(@RequestBody RegRequest regRequest) {
         String email = regRequest.getEmail();
         String password = regRequest.getPassword();
         String pin = regRequest.getPin();
-        if(email==null||password==null||pin==null)
+        if (email == null || password == null || pin == null)
             return new Result<>("传入参数不足", 201);
-        return regService.Reg(email,password,pin);
+        return regService.Reg(email, password, pin);
+    }
+
+    @PostMapping("/login")
+    public Result<List<User>> login(@RequestBody LoginRequest loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        if (email.isEmpty() || password.isEmpty()) {
+            return new Result<>("传入参数不足", 201);
+        }
+        return loginService.login(email,password);
     }
 }
