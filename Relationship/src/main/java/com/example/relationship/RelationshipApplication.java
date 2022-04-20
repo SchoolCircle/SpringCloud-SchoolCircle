10d4635c1306b42cb592feb22d6a737eb5cc9eaf
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-04-20 13:56:29 +0800

    搭建relationship模块

--- Relationship/src/main/java/com/example/relationship/RelationshipApplication.java
+++ Relationship/src/main/java/com/example/relationship/RelationshipApplication.java
@@ -2,8 +2,10 @@ package com.example.relationship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RelationshipApplication {
     public static void main(String[] args) {
         SpringApplication.run(RelationshipApplication.class, args);
