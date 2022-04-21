0f3c7a5e9ca6e03288b9a6e88d8476d2f5ccc4c2
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-04-21 15:21:10 +0800

    打包失败

--- Common-api/src/main/java/com/example/entity/User.java
+++ Common-api/src/main/java/com/example/entity/User.java
@@ -2,14 +2,14 @@ package com.example.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+
 @Data
 @Entity
+@Table(name="user")
 public class User {
 
     @Id
