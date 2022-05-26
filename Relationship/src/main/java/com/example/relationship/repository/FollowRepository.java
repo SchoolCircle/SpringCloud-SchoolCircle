d66b615b7b3ff1c76c6b08fa9dce2c0a1ec021ce
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-05-26 23:34:04 +0800

    增加关注和取关的接口

--- Relationship/src/main/java/com/example/relationship/repository/FollowRepository.java
+++ Relationship/src/main/java/com/example/relationship/repository/FollowRepository.java
@@ -9,4 +9,9 @@ public interface FollowRepository extends JpaRepository<Follow, Integer> {
     List<Follow> findFollowsByFollowed(Integer followed);
 
     List<Follow> findFollowsByFollowing(Integer following);
+
+    // 判断有无关注信息
+    boolean existsFollowsByFollowedAndAndFollowing(Integer followed, Integer following);
+
+    List<Follow> findFollowsByFollowedAndFollowing(Integer followed, Integer following);
 }
--- Relationship/src/main/java/com/example/relationship/service/FollowService.java
+++ Relationship/src/main/java/com/example/relationship/service/FollowService.java
@@ -2,11 +2,15 @@ package com.example.relationship.service;
 
 import com.example.relationship.entity.Follow;
 import com.example.relationship.entity.Result;
+import com.example.relationship.entity.User;
 import com.example.relationship.repository.FollowRepository;
 import com.example.relationship.repository.UserRepository;
+import com.example.relationship.utils.Sdf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,6 +22,13 @@ public class FollowService {
     @Autowired
     private UserRepository userRepository;
 
+    //检查token
+    private boolean checkToken(String token, Integer uid){
+        List<User> list = userRepository.findUsersByUid(uid);
+        if(list.isEmpty()) return false;
+
+        return list.get(0).getToken().equals(token); //token 匹配否？
+    }
     /**
      * 返回关注传入uid的人的列表
      * @param uid 被关注的uid
@@ -42,5 +53,52 @@ public class FollowService {
         return new Result<>(followRepository.findFollowsByFollowing(uid));
     }
 
+    /**
+     *
+     * @param followed 关注者
+     * @param following 被关注者
+     * @param token 关注者的token
+     * @return 关注情况
+     */
+    public Result<List<Follow>> addFollow(Integer followed, Integer following, String token){
+        if(!userRepository.existsUserByUid(followed)||!userRepository.existsUserByUid(following)){
+            return new Result<>("用户不存在", 201);
+        }
+        if(!checkToken(token, following)){
+            return new Result<>("无权操作", 201);
+        }//关注操作是由following执行的，检查其权限
+        if(followRepository.existsFollowsByFollowedAndAndFollowing(followed,following)){
+            return new Result<>("已关注",201);
+        }
+        Follow follow = new Follow();
+        follow.setFollowed(followed);
+        follow.setFollowing(following);
+        follow.setFollowTime(Sdf.sdf.format(new Date()));//当前时间
+        follow = followRepository.save(follow);
+        return new Result<>(Collections.singletonList(follow));
+    }
+
+    /**
+     *
+     * @param followed 被关注者
+     * @param following 关注者
+     * @param token 关注者token
+     * @return 是否成功， 200为成功, 201不成功
+     */
+    public Result<List<Follow>> delFollow(Integer followed, Integer following, String token){
+        if(!userRepository.existsUserByUid(followed)||!userRepository.existsUserByUid(following)){
+            return new Result<>("用户不存在", 201);
+        }
+        if(!checkToken(token, following)){
+            return new Result<>("无权操作", 201);
+        }//取关操作是由following执行的，检查其权限
+        List<Follow> list = followRepository.findFollowsByFollowedAndFollowing(followed,following);
+        if(list.isEmpty()){
+            return new Result<>("未关注", 201);
+        }
+        Follow follow = list.get(0);
+        followRepository.delete(follow);
+        return new Result<>("success", 200);
+    }
 
 }
